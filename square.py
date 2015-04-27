fa39bec6f4579bb54bc3b4041716319aa8f7712f
Andy <el.andy.barr@gmail.com>
2015-04-26 18:18:10 -0700

    cubes

--- square.py
+++ square.py
@@ -2,8 +2,13 @@ global y
 
 def square(x):
 	y = x*x
+	print('{0} squared is {1}'.format(x,y))
 	return(y)
 	
 c = square(10)
 print(c)
-print(y)
+
+def cube(x):
+	y = x*x*x
+	print('{0} cubed is {1}'.format(x,y))
+	return(y)
